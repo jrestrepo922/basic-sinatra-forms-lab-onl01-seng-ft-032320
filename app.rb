dd11bdb01d49e3a1fb01944551b03aa4314c5451
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-30 11:44:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,7 +9,10 @@ class App < Sinatra::Base
   post '/team'do 
     @team_name = params[:team_name] 
     @coach = params[:coach]
-    @point_guard = para
+    @point_guard = params[:point_guard]
+    @small_forward = params[:small_forward]
+    @power_foward = params[:power_foward]
+    @center = params[:center]
     erb :team 
   end 
 end
