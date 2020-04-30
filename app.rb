0645f919a9c982717f6ba0dec057fd19a6e55913
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-30 11:55:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,6 +10,7 @@ class App < Sinatra::Base
     @team_name = params[:name] 
     @coach = params[:coach]
     @point_guard = params[:pg]
+    @shooting_guard = params[:sg]
     @small_forward = params[:sf]
     @power_foward = params[:pf]
     @center = params[:c]
